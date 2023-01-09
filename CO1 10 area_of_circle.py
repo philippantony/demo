1bef28612536be29c79858f7daead3e38cfd1ebf
philippantony <117849650+philippantony@users.noreply.github.com>
2023-01-09 10:40:06 +0530

    c01

--- CO1 10 area_of_circle.py
+++ CO1 10 area_of_circle.py
@@ -1,5 +1,6 @@
 r = int(input("Enter the radius  :"))
 area = 3.14*r*r  #calculating area
 print('Area of the Circle is :', area)
+
 pr = 2*3.14*r   #calculating perimeter
 print('Perimeter of the Circle is :', pr)
